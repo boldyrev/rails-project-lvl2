1f720d7f8a67559c20c486581c9e57561f51b4ab
Aleksey Boldyrev <aleksey.boldyrev@gmail.com>
2022-05-02 13:21:55 +0300

    Add likes counter cache

---
+++ db/migrate/20220502101122_reset_all_post_cache_counters.rb
@@ -0,0 +1,9 @@
+class ResetAllPostCacheCounters < ActiveRecord::Migration[6.1]
+  def up
+    Post.all.each do |post|
+      Post.reset_counters(post.id, :likes)
+    end
+  end
+
+  def down; end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_02_052135) do
+ActiveRecord::Schema.define(version: 2022_05_02_101122) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2022_05_02_052135) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "likes_count"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
